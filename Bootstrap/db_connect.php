6b1ec2c4ef98014d5f18b945ad27827c439a7c5f
zahinwatanmall <whasango_1983@Zahins.local>
2015-01-14 13:30:48 +0100

    First version

--- Bootstrap/db_connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$db = new PDO('mysql:host=localhost;dbname=jensen', 'root', '');		//Databas kopplingen
-$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 		//Inställningar för error hantering
-$db->exec("SET NAMES 'utf8'");										//Teckenkod
-
-?>
